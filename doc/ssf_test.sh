4e013d7da7c0bd659d2cd2f6a2778210acbacdcd
acatspirit <acatspirit@gmail.com>
2025-07-22 11:06:35 -0400

    alter the .sh file

--- doc/ssf_test.sh
+++ doc/ssf_test.sh
@@ -6,5 +6,5 @@
 #SBATCH --mail-type=END        
 #SBATCH --mail-user=am1155@duke.edu       # It will send you an email when the job is finishe$
 #SBATCH --mem=10G                # Memory, keep it as 10G
-#SBATCH --job-name=ssf_test_code_cap.out                # How is your output is called, you n$
+#SBATCH --job-name=ssf_test_circ_level.out                # How is your output is called, you n$
 python3 ssf_test.py 
